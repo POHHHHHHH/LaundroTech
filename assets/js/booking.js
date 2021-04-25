8a211e53ab76768f532b7fe4706509c9738c957d
Jia Ming <wongjiaming93@gmail.com>
2021-04-25 16:53:01 +0800

    review.html

---
+++ assets/js/booking.js
@@ -0,0 +1,188 @@
+window.addEventListener( "pageshow", function ( event ) {
+    var historyTraversal = event.persisted || 
+                           ( typeof window.performance != "undefined" && 
+                                window.performance.navigation.type === 2 );
+    if ( historyTraversal ) {
+      // Handle page restore.
+      window.location.reload();
+    }
+  });
+  
+Date.prototype.addHours = function(h){
+    this.setTime(this.getTime() + (h*60*60*1000));
+
+    return this;
+}
+
+function selectDate(){
+
+    var today = new Date().addHours(8).toISOString().split('T')[0];
+    document.getElementsByName("bookingDate")[0].setAttribute('min', today);
+
+}
+window.onload = selectDate;
+
+function changeTimeslot(){
+
+    var today = new Date().addHours(8).toISOString().split('T')[0];
+
+    document.getElementsByName("bookingDate")[0].setAttribute('min', today);
+
+    var date = new Date(document.getElementById("bookingDate").value);
+    var day = date.getDate();
+    if(day < 10){
+        day = "0" + day;
+    }
+    var month = date.getMonth()+1;
+    if(month < 10){
+        month = "0"+ month;
+    }
+    var year = date.getFullYear();
+
+    var selectedDate = day + "/" + month + "/" + year;
+    var myHeaders = new Headers(); 
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify({"query":"SELECT ts.bookingTime FROM (SELECT t.bookingTime, w.washingMachineID FROM laundrotech.Booking AS b, laundrotech.WashingMachine AS w, laundrotech.Timeslot AS t " + 
+        "WHERE b.FK_washingMachineID = w.washingMachineID AND w.location= 'pioneer' AND b.FK_timeslotID <> t.timeslotID AND b.bookingDate = '" + selectedDate + "' " + 
+        "UNION " + 
+        "SELECT t.bookingTime, w.washingMachineID FROM laundrotech.Booking AS b, laundrotech.WashingMachine AS w, laundrotech.Timeslot AS t " + 
+        "WHERE (w.washingMachineID NOT IN (SELECT bk.FK_washingMachineID FROM laundrotech.Booking AS bk WHERE bk.bookingDate = '" + selectedDate +"')) AND w.location= 'pioneer') AS ts " + 
+        "GROUP BY ts.bookingTime"});
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+    };
+
+    var str = "";
+    fetch("https://3rczj928aa.execute-api.us-east-1.amazonaws.com/prod/gettimeslot", requestOptions)
+    .then(response => response.text())
+    .then(result => {
+         var data = JSON.parse(result);
+            str += "<option value='chooseTimeslot' selected disabled hidden>Choose a Timeslot</option>";
+            for (var i = 0; i < data.length; i++) {
+            str += "<option>" + data[i].bookingTime + "</option>"
+        }
+        document.getElementById("bookingTime").innerHTML = str;
+        var selectElement = document.getElementById("availWMTable");
+        selectElement.style.display = "none";
+    })
+    .catch(error => console.log('error', error));
+}
+
+function displayTable(){
+    var date = new Date(document.getElementById("bookingDate").value);
+    var day = date.getDate();
+    if(day < 10){
+        day = "0" + day;
+    }
+    var month = date.getMonth()+1;
+    if(month < 10){
+        month = "0"+ month;
+    }
+    var year = date.getFullYear();
+
+    var selectedDate = day + "/" + month + "/" + year;
+    var timeslot = document.getElementById("bookingTime").value;
+
+    var myHeaders = new Headers(); 
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify({"query":"SELECT wm.washingMachineID, wm.washingMachineBrand, wm.washingMachineModel, wm.price, wm.washingMachineAddress FROM " + 
+                    "(SELECT t.bookingTime, w.washingMachineID, w.washingMachineBrand, w.washingMachineModel, w.price, w.washingMachineAddress FROM laundrotech.Booking AS b, laundrotech.WashingMachine AS w, laundrotech.Timeslot AS t " + 
+                    "WHERE b.FK_washingMachineID = w.washingMachineID AND w.location= 'pioneer' AND b.FK_timeslotID <> t.timeslotID AND b.bookingDate = '" + selectedDate + "' " + 
+                    "UNION " + 
+                    "SELECT t.bookingTime, w.washingMachineID, w.washingMachineBrand, w.washingMachineModel, w.price, w.washingMachineAddress FROM laundrotech.Booking AS b, laundrotech.WashingMachine AS w, laundrotech.Timeslot AS t " + 
+                    "WHERE (w.washingMachineID NOT IN (SELECT bk.FK_washingMachineID FROM laundrotech.Booking AS bk WHERE bk.bookingDate = '" + selectedDate + "')) AND w.location= 'pioneer') AS wm " + 
+                    "WHERE wm.bookingTime = '" + timeslot + "' GROUP BY wm.washingMachineID"});
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+    };
+
+    var str = "";
+    str += '<table class = "availWM">' + 
+            '<thead>' + 
+                '<tr>' +
+                '<th><strong>Washing Machine No.</strong></th>' +
+                '<th><strong>Washing Machine Brand</strong></th>' +
+                '<th><strong>Washing Machine Model</strong></th>' +
+                '<th><strong>Price</strong></th>' +
+                '<th><strong>Address</strong></th>' +
+                '</tr>' +
+            '</thead>' +
+            '<tbody">';
+
+
+    fetch("https://3rczj928aa.execute-api.us-east-1.amazonaws.com/prod/getavailwm", requestOptions)
+    .then(response => response.text())
+    .then(result => {
+         var data = JSON.parse(result);
+         
+            for (var i = 0; i < data.length; i++) {
+            str += "<tr onclick=storeWMDetails(this);>" +  
+                "<td>" + data[i].washingMachineID + "</td>" + 
+                "<td>" + data[i].washingMachineBrand + "</td>" +
+                "<td>" + data[i].washingMachineModel + "</td>" +
+                "<td>" + data[i].price + "</td>" +
+                "<td>" + data[i].washingMachineAddress + "</td>" +
+                "</a>" + "</tr>"
+        }
+
+        str += "</tbody>" + "</tables>";
+        document.getElementById("availWMTable").innerHTML = str;
+        var selectElement = document.getElementById("availWMTable");
+        selectElement.style.display = "block";
+    })
+    .catch(error => console.log('error', error));
+}
+
+function storeWMDetails(row){
+    var date = new Date(document.getElementById("bookingDate").value);
+    var day = date.getDate();
+    if(day < 10){
+        day = "0" + day;
+    }
+    var month = date.getMonth()+1;
+    if(month < 10){
+        month = "0"+ month;
+    }
+    var year = date.getFullYear();
+
+    var selectedDate = day + "/" + month + "/" + year;
+    var timeslot = document.getElementById("bookingTime").value;
+
+    sessionStorage.setItem('userID', '1');
+    sessionStorage.setItem("location", "pioneer");
+    sessionStorage.setItem("washingMachineID", row.cells[0].innerHTML);
+    sessionStorage.setItem("price", row.cells[3].innerHTML);
+    sessionStorage.setItem("washingMachineAddres", row.cells[4].innerHTML);
+    sessionStorage.setItem("bookingDate", selectedDate);
+    sessionStorage.setItem("bookingTime", timeslot);
+
+    var myHeaders = new Headers(); 
+    myHeaders.append("Content-Type", "application/json");
+    
+    var raw = JSON.stringify({"query":"SELECT timeslotID FROM laundrotech.Timeslot WHERE bookingTime = '" + timeslot + "'"});
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+    };
+
+    fetch("https://3rczj928aa.execute-api.us-east-1.amazonaws.com/prod/gettimeslot", requestOptions)
+    .then(response => response.text())
+    .then(result => {
+         var data = JSON.parse(result);
+        sessionStorage.setItem("timeslotID", data[0].timeslotID);
+        window.location.href = "review.html";
+    })
+    .catch(error => console.log('error', error));
+    
+    
+}
---
+++ assets/js/review.js
@@ -0,0 +1,148 @@
+function loadReview(){
+    var myHeaders = new Headers(); 
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify({"query":"SELECT username, fullName, email, contactNo, credit FROM laundrotech.User WHERE userID = '" + sessionStorage.getItem("userID") + "'"});
+
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+    };
+
+    var str = "";
+    fetch("https://3rczj928aa.execute-api.us-east-1.amazonaws.com/prod/reviewbooking", requestOptions)
+    .then(response => response.text())
+    .then(result => {
+        var data = JSON.parse(result);
+        str = "<span class=\"reviewBooking\"><label class=\"reviewLabel\"><strong>Full Name:</strong></label><label id=\"fullName\" class=\"reviewValues\">" + data[0].fullName + "</label></span>";
+        document.getElementById("spanFullName").innerHTML = str;
+        str = "<span class=\"reviewBooking\"><label class=\"reviewLabel\"><strong>Username:</strong></label><label id=\"userName\" class=\"reviewValues\">" + data[0].username + "</label></span>";
+        document.getElementById("spanUserName").innerHTML = str;
+        str = "<span class=\"reviewBooking\"><label class=\"reviewLabel\"><strong>Email:</strong></label><label id=\"email\" class=\"reviewValues\">" + data[0].email + "</label></span>";
+        document.getElementById("spanEmail").innerHTML = str;
+        str = "<span class=\"reviewBooking\"><label class=\"reviewLabel\"><strong>Contact No:</strong></label><label id=\"contactNo\" class=\"reviewValues\">" + data[0].contactNo + "</label></span>";
+        document.getElementById("spanContactNo").innerHTML = str;
+        str = "<span class=\"reviewBooking\"><label class=\"reviewLabel\"><strong>Credits remaining:</strong></label><label id=\"creditsRemain\" class=\"reviewValues\">" + data[0].credit + "</label></span>";
+        document.getElementById("spanCreditsRemain").innerHTML = str;
+        sessionStorage.setItem("credit", data[0].credit);
+        str = "<span class=\"reviewBooking\"><label class=\"reviewLabel\"><strong>Washing Machine Price:</strong></label><label id=\"price\" class=\"reviewValues\">$" + sessionStorage.getItem("price") + "</label></span>";
+        document.getElementById("spanPrice").innerHTML = str;
+        str = "<span class=\"reviewBooking\"><label class=\"reviewLabel\"><strong>Booking Fee:</strong></label><label id=\"bookingFee\" class=\"reviewValues\">$1</label></span>";
+        document.getElementById("spanBookingFee").innerHTML = str;
+        str = "<span class=\"reviewBooking\"><label class=\"reviewLabel\"><strong>Total:</strong></label><label id=\"total\" class=\"reviewValues\">$" + (parseInt(sessionStorage.getItem("price")) + 1) + "</label></span>";
+        sessionStorage.setItem("total", (parseInt(sessionStorage.getItem("price")) + 1).toString());
+        document.getElementById("spanTotal").innerHTML = str;
+        str = "<div id=\"reviewButton\" class=\"reviewButton\"><input type=\"button\" id=\"topUp\" value=\"Top Up Credits\" onclick=\"location.href='topup.html'\"><input type=\"button\" id=\"checkout\" value=\"Checkout\" onclick=\"checkout()\"></div>";
+        document.getElementById("reviewButton").innerHTML = str;
+    })
+    .catch(error => console.log('error', error));				
+}
+
+window.onload=loadReview;
+
+function checkout(){
+
+    if(parseInt(sessionStorage.getItem("total")) > sessionStorage.getItem("credit")){
+        var insufficientFunds = document.getElementById("insufficientCredits");
+        insufficientFunds.style.display = "block";
+    }
+    else{
+        var myHeaders = new Headers(); 
+        myHeaders.append("Content-Type", "application/json");
+
+        var raw = JSON.stringify({"query":"SELECT bookingCode FROM laundrotech.Booking WHERE bookingDate = '" + sessionStorage.getItem("bookingDate") + "'"});
+
+        var requestOptions = {
+            method: 'POST',
+            headers: myHeaders,
+            body: raw,
+            redirect: 'follow'
+        };
+
+        var str = "";
+        
+        fetch("https://3rczj928aa.execute-api.us-east-1.amazonaws.com/prod/reviewbooking", requestOptions)
+        .then(response => response.text())
+        .then(result => {
+            var data = JSON.parse(result);
+            var bookingCode = '';
+            if(data[0] == null){
+                
+                chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+                for (var i = 5; i > 0; --i) 
+                    bookingCode += chars[Math.floor(Math.random() * chars.length)];
+            }
+            else{
+                var codeExists = true;
+                while(codeExists){
+                    bookingCode = '';
+                    var count = '0';
+                    chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+                    for (var i = 5; i > 0; --i) 
+                        bookingCode += chars[Math.floor(Math.random() * chars.length)];
+
+                    for(var i = 0; i < data.length; i++){
+                        if(data[i].bookingCode == bookingCode){
+                            count = '1';
+                            i = data.length;
+                        }
+                    }
+
+                    if(count == '0'){
+                        codeExists = false;
+                    }
+                    else{
+                        count = '0';
+                    }
+                }
+            }
+
+            var startHr = parseInt(sessionStorage.getItem("bookingTime").substring(0,2));
+            
+            var endHr = startHr + 1;
+            
+            if(endHr < 10){
+                endHr = "0" + endHr.toString() + ":00";
+            }
+            else if(endHr == 24){
+                endHr = "00:00";
+            }
+            else{
+                endHr = endHr.toString() + ":00";
+            }
+
+            raw = JSON.stringify({"query":"INSERT INTO laundrotech.Booking (FK_userID,FK_timeslotID,FK_washingMachineID,bookingDate,startTime,endTime,bookingStatus,bookingCode) VALUES (" + 
+                        sessionStorage.getItem("userID") + "," + sessionStorage.getItem("timeslotID") + "," + sessionStorage.getItem("washingMachineID") + ",'" + sessionStorage.getItem("bookingDate") + "','" + sessionStorage.getItem("bookingTime") + "','" + 
+                        endHr + "'," + "'active','" + bookingCode + "');"});
+            requestOptions = {
+                method: 'POST',
+                headers: myHeaders,
+                body: raw,
+                redirect: 'follow'
+            };
+
+            alert(raw);
+            return fetch("https://3rczj928aa.execute-api.us-east-1.amazonaws.com/prod/reviewbooking", requestOptions)
+        })
+        .then(response => response.text())
+        .then(result => {
+            raw = JSON.stringify({"query":"UPDATE laundrotech.User SET credit = " + (sessionStorage.getItem("credit") - parseInt(sessionStorage.getItem("total"))) + " WHERE userID = " + sessionStorage.getItem("userID") + ";"});
+
+            requestOptions = {
+                method: 'POST',
+                headers: myHeaders,
+                body: raw,
+                redirect: 'follow'
+            };
+
+            alert(raw);
+            return fetch("https://3rczj928aa.execute-api.us-east-1.amazonaws.com/prod/reviewbooking", requestOptions)
+        })
+        .then(response => response.text())
+        .then(result => window.location.replace("index.html"))
+        .catch(error => console.log('error', error));
+    }
+    
+}
