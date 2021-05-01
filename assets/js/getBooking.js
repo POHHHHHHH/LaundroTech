3fe4e5497750deef5c1fcd6294b895ad7a28831e
chunpohl@gmail.com <chunpohl@gmail.com>
2021-05-02 03:24:11 +0800

    added account inforamtion in getBooking.html so as to get credits
    added booking status in completed/ refunded booking

--- assets/js/getBooking.js
+++ assets/js/getBooking.js
@@ -17,28 +17,58 @@ function getActiveBooking() {
         .then(result => {
             var data = JSON.parse(result);
 
-            console.log("null")
-            console.log("else");
-
             str += "<table style=\"width:100%\"> <tr><th>Booking ID</th><th>Booking Date</th><th>Start Time</th><th>End Time</th><th>Booking Code</th><th>Cancel Booking</th></tr>"
             for (var i = 0; i < data.length; i++) {
                 count = i + 1;
-                str += "<tr><th>" + data[i].bookingID + "</th></th><th>" + data[i].bookingDate + "</th><th>" + data[i].startTime + "</th><th>" + data[i].endTime + "</th><th>" + data[i].bookingCode + "</><th><article class='style" + "'>" + "</span>" + "<button onclick='callMethod(" + data[i].bookingID + ")'>DELETE</button></th></tr>"
+                str += "<tr><th>" + data[i].bookingID + "</th></th><th>" + data[i].bookingDate + "</th><th>" + data[i].startTime + "</th><th>" + data[i].endTime + "</th><th>" + data[i].bookingCode + "</><th><article class='style" + "'>" + "</span>" + "<button onclick='callMethod(" + data[i].bookingID + ")'>Cancel </button></th></tr>"
             }
             str += "</table>"
             document.getElementById("locationTile").innerHTML = str;
             getCompletedBooking();
+            loadReview();
 
         })
         .catch(error => console.log('error', error));
 
 }
 
+function loadReview(){
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify({"query":"SELECT username, fullName, email, contactNo, credit FROM laundrotech.User WHERE userID = '" + sessionStorage.getItem("userID") + "'"});
+
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+    };
+
+    var str = "";
+    fetch("https://3rczj928aa.execute-api.us-east-1.amazonaws.com/prod/getbooking", requestOptions)
+        .then(response => response.text())
+        .then(result => {
+            var data = JSON.parse(result);
+            str = "<span class=\"reviewBooking\"><label class=\"reviewLabel\"><strong>Full Name:</strong></label><label id=\"fullName\" class=\"reviewValues\">" + data[0].fullName + "</label></span>";
+            document.getElementById("spanFullName").innerHTML = str;
+            str = "<span class=\"reviewBooking\"><label class=\"reviewLabel\"><strong>Username:</strong></label><label id=\"userName\" class=\"reviewValues\">" + data[0].username + "</label></span>";
+            document.getElementById("spanUserName").innerHTML = str;
+            str = "<span class=\"reviewBooking\"><label class=\"reviewLabel\"><strong>Email:</strong></label><label id=\"email\" class=\"reviewValues\">" + data[0].email + "</label></span>";
+            document.getElementById("spanEmail").innerHTML = str;
+            str = "<span class=\"reviewBooking\"><label class=\"reviewLabel\"><strong>Contact No:</strong></label><label id=\"contactNo\" class=\"reviewValues\">" + data[0].contactNo + "</label></span>";
+            document.getElementById("spanContactNo").innerHTML = str;
+            str = "<span class=\"reviewBooking\"><label class=\"reviewLabel\"><strong>Credits remaining:</strong></label><label id=\"creditsRemain\" class=\"reviewValues\">" + data[0].credit + "</label></span>";
+            document.getElementById("spanCreditsRemain").innerHTML = str;
+        })
+        .catch(error => console.log('error', error));
+}
+
 function getCompletedBooking() {
     var getUserID = sessionStorage.getItem("userID");
     var myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
-    var raw = JSON.stringify({ "query": "SELECT bookingID,bookingDate,startTime,endTime, bookingCode FROM laundrotech.Booking where bookingStatus in ('completed','canceled') and FK_userID = " + getUserID + " ;" });
+    var raw = JSON.stringify({ "query": "SELECT * FROM laundrotech.Booking where bookingStatus in ('completed','canceled') and FK_userID = " + getUserID + " ;" });
     console.log(raw);
     var requestOptions = {
         method: 'POST',
@@ -57,10 +87,10 @@ function getCompletedBooking() {
             } else {
                 console.log("else");
 
-                str += "<table style=\"width:100%\"> <tr><th>Booking ID</th><th>Booking Date</th><th>Start Time</th><th>End Time</th><th>Booking Code</th></tr>"
+                str += "<table style=\"width:100%\"> <tr><th>Booking ID</th><th>Booking Date</th><th>Start Time</th><th>End Time</th><th>Booking Status</th><th>Booking Code</th></tr>"
                 for (var i = 0; i < data.length; i++) {
                     count = i + 1;
-                    str += "<tr><th>" + data[i].bookingID + "</th><th>" + data[i].bookingDate + "</th><th>" + data[i].startTime + "</th><th>" + data[i].endTime + "</th><th>" + data[i].bookingCode + "</th></tr>"
+                    str += "<tr><th>" + data[i].bookingID + "</th><th>" + data[i].bookingDate + "</th><th>" + data[i].startTime + "</th><th>" + data[i].endTime + "</th><th>" + data[i].bookingStatus + "</th><th>" + data[i].bookingCode + "</th></tr>"
                 }
                 str += "</table>"
                 document.getElementById("locationTile2").innerHTML = str;
@@ -72,7 +102,7 @@ function getCompletedBooking() {
 }
 
 function callMethod(bookingId) {
-    var result = confirm("要delete吗");
+    var result = confirm("Are you sure you wish to Cancel Booking");
     if (result) {
         updateCancel(bookingId);
         var userID = sessionStorage.getItem("userID");
