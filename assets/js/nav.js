3e5781dc902e3aed9e8d5a7cc89263a015c58627
chunpohl@gmail.com <chunpohl@gmail.com>
2021-05-04 15:06:58 +0800

    updated index with login message

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -4,6 +4,19 @@ window.addEventListener('load', function () {
 
     var currentLocation = window.location.href;
 
+    if(userID === null && currentLocation.includes("index.html")){
+        str2 = '';
+        str2="<li><h2><a href=\"Login.html\">Get Started</a></li></h2>";
+    }
+    else if(sessionStorage.getItem("role") === "admin" && currentLocation.includes("index.html")){
+        str2="<li><h2>Welcome Admin</a></li></h2>";
+    }
+    else{
+        str2="<li><h2><a href=\"getLocation.html\">Start Booking</a></li></h2>";
+    }
+    var nav2 = document.getElementById("login");
+    nav2.innerHTML = str2;
+
     if (userID === null && (!(currentLocation.includes("login.html") || currentLocation.includes("index.html") || currentLocation === "https://master.d8loh5zuotepy.amplifyapp.com/"))) {
         alert("Please login before proceeding!")
         window.location.replace("login.html")
